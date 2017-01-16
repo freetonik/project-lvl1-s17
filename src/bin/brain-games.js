d529175d97e6d7506b9583b70e34c75cfaea2a97
Rakhim Davletkaliyev <freetonik@gmail.com>
2017-01-16 14:26:07 +0200

    add readline-sync

--- src/bin/brain-games.js
+++ src/bin/brain-games.js
@@ -1,1 +1,5 @@
-console.log("Welcome to the Brain Games!");
+import readlineSync from 'readline-sync';
+
+console.log('Welcome to the Brain Games!');
+const name = readlineSync.question('May I have your name?: ');
+console.log(`Hello, ${name}`);
